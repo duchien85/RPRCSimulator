391328b8dd916e020c9770bd84692474611f5198
Predrag Cokulov <predragcokulov@gmail.com>
2015-06-07 12:58:33 +0200

    receiving status and power

--- core/src/com/androidforever/rprcsimulator/Simulator.java
+++ core/src/com/androidforever/rprcsimulator/Simulator.java
@@ -1,7 +1,9 @@
 package com.androidforever.rprcsimulator;
 
-import android.widget.*;
-import com.androidforever.rprccommon.lib.*;
+import com.androidforever.rprccommon.lib.KryoNet;
+import com.androidforever.rprccommon.lib.Position;
+import com.androidforever.rprccommon.lib.Power;
+import com.androidforever.rprccommon.lib.Status;
 import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.*;
@@ -10,6 +12,7 @@ import com.badlogic.gdx.math.*;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.physics.box2d.joints.*;
 import com.esotericsoftware.kryonet.*;
+
 import java.io.*;
 
 public class Simulator extends ApplicationAdapter
@@ -19,9 +22,9 @@ public class Simulator extends ApplicationAdapter
     public static float ASPECT_RATIO;
     public static final float DRAW_WIDTH = 12.444f;
     public static final float PROPELLER_HEIGHT = 1.5f;
-	public static final float MAX_PROPELLER_SPEED = 4712.387f;//rad/s, 45000 rpm
-	public static final float MIN_PROPELLER_SPEED = 104.7197f;//rad/s, 1000 rpm, this is a guess
-	public static final float PROPELLER_TORQUE = 1000f;
+    public static final float MAX_PROPELLER_SPEED = 100f;
+    public static final float MIN_PROPELLER_SPEED = 20f;
+    public static final float PROPELLER_TORQUE = 1000f;
 
     static Texture propellerTexture;
     OrthographicCamera cam;
@@ -29,6 +32,9 @@ public class Simulator extends ApplicationAdapter
     ShapeRenderer shapeRenderer;
     World world;
     Box2DDebugRenderer debugRenderer;
+    BitmapFont debugFont;
+
+    private float currentAcPower = 0;
 
 
     @Override
@@ -45,54 +51,149 @@ public class Simulator extends ApplicationAdapter
 
         world = new World(new Vector2(0, 0), true);
         debugRenderer = new Box2DDebugRenderer();
-		
-		final Server server = KryoNet.createServer();
-		server.start();
-		new Thread(new Runnable(){
 
-				@Override
-				public void run()
-				{
-					try
-					{
-						server.bind(KryoNet.TCP_PORT, KryoNet.UDP_PORT);
-						server.addListener(new Listener()
-							{
-								@Override
-								public void received(Connection con, Object obj)
-								{
-									if(obj instanceof Status)
-									{
-										Status status = (Status)obj;
-										if(status.request)
-										{
-											con.sendTCP(new Status(status.id, false, Propeller.getProp1(cam, world).joint.getMotorSpeed() > 0));
-										}
-										else
-										{
-											boolean power = status.power;
-											setSpeedAll(power ? MIN_PROPELLER_SPEED : 0);
-											con.sendTCP(new Status(status.id, false, true));
-										}
-									}
-								}
-							});
-					}
-					catch (IOException e)
-					{
-						//todo binding server failed
-						e.printStackTrace();
-					}
-				}
-				
-			
-		}).start();
-		
+        debugFont = new BitmapFont();
+        debugFont.setColor(Color.RED);
+        debugFont.getData().setScale(0.03f);
+
+        final Server server = KryoNet.createServer();
+        server.start();
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    server.bind(KryoNet.TCP_PORT, KryoNet.UDP_PORT);
+                    server.addListener(new Listener()
+                    {
+                        @Override
+                        public void received(Connection con, Object obj)
+                        {
+                            if (obj instanceof Status)
+                            {
+                                Status status = (Status) obj;
+                                if (status.request)
+                                {
+                                    con.sendTCP(new Status(false, Propeller.getProp1(cam, world).joint.getMotorSpeed() > 0));
+                                }
+                                else
+                                {
+                                    boolean power = status.power;
+                                    setSpeedAll(power ? MIN_PROPELLER_SPEED : 0);
+                                }
+                            }
+                            else if(obj instanceof Power)
+                            {
+                                Power power = (Power) obj;
+                                float range = MAX_PROPELLER_SPEED - MIN_PROPELLER_SPEED;
+                                if (power.request)
+                                {
+                                    int pwr = Propeller.getProp1(cam, world).joint.getMotorSpeed() < MIN_PROPELLER_SPEED
+                                            ? 0
+                                            : (int) (Math.abs(MIN_PROPELLER_SPEED - Propeller.getProp1(cam, world).joint.getMotorSpeed()) / range * 100);
+                                    con.sendTCP(new Power(false, pwr));
+                                }
+                                else
+                                {
+                                    int powerPerc = power.power;
+                                    float pwr = MIN_PROPELLER_SPEED + (range / 100 * powerPerc);
+                                    currentAcPower = pwr;
+                                    setSpeedAll(pwr);
+                                    //TODO consider position
+                                }
+                            }
+                            else if(obj instanceof Position)
+                            {
+                                Position pos = (Position) obj;
+                                int angle = pos.angle;//in degrees
+                                int power = pos.power;//0-100 %
+                                float speedRange = MAX_PROPELLER_SPEED - MIN_PROPELLER_SPEED;
+
+                                //if anyone is reading this, please bear in mind that i have no idea what
+                                //I am talking about. These are just my random thoughts, logical
+                                //thinking about the solutions :D... brainstorming...
+
+                                //this is actually mapping 2D coordinate system from controllers joystick to
+                                //3D position of the aircraft
+
+                                //speedRange - for this simulation, it is value between max and min speed
+                                //             for real quad-copter it is value between max and min rpm
+
+                                //power      - percent 0-100% for turning power, 100 means maximum possible turn
+                                //           - this is multiplier for angle...
+
+                                //angle      - this one is complicated, random thoughts follows
+                                //           - we will calculate propellers/motors power decrease based on angle
+                                //           - range from '-180' to '180'.
+                                //           - from '0' - '180' - right side of the joystick
+                                //           - from '0' - '-180' - left side of the joystick
+                                //           - Math.abs(angle) > 90 - down side of the joystick
+                                //           - Math.abs(angle) < 90 - up side of the joystick
+
+                                //ultimately we need to convert this values to 'pitch', 'roll' and 'yaw'
+
+                                //pitch (z) - moving up-down with front (eg. nose) - on an axis running from
+                                //            middle-left to middle-right of the ac (aircraft)
+                                //yaw   (y) - moving left-right - on an axis running up and down (roof to floor)
+                                //roll  (x) - rotating on axis running from middle-front to middle-back
+
+                                //we do that by calculating propeller speed for all 4 propellers
+                                //we will do this by decrease speed of appropriate propeller/propellers based on angle and power
+                                //so for example if we have to turn right, right two propellers have to be decreased by power %
+                                //...
+                                //its a lot more complicated than it sounds...
+
+                                //here we go...
+
+                                if(power == 0)
+                                {
+                                    //levelled
+                                    //reset all motors to same value, level the ac
+                                    setSpeedAll(currentAcPower);
+                                    return;
+                                }
+
+                                float prop1Speed = Propeller.getProp1(cam, world).joint.getMotorSpeed();//top left
+                                float prop2Speed = Propeller.getProp2(cam, world).joint.getMotorSpeed();//bottom left
+                                float prop3Speed = Propeller.getProp3(cam, world).joint.getMotorSpeed();//top right
+                                float prop4Speed = Propeller.getProp4(cam, world).joint.getMotorSpeed();//bottom right
+
+                                //first determine if we are ascending or descending
+                                if(Math.abs(angle) > 90)//joystick bottom, ascending nose up
+                                {
+                                    //power is increasing the speed
+                                    if(angle > 0)//right
+                                    {
+
+                                    }
+                                    else//left
+                                    {
+
+                                    }
+                                }
+                                else//joystick top, descending nose down
+                                {
+                                    //power is decreasing the speed
+                                }
+
+                            }
+                        }
+                    });
+                }
+                catch (IOException e)
+                {
+                    //todo binding server failed
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     private void initCamera()
     {
-        ASPECT_RATIO = (float)Gdx.graphics.getWidth()/(float)Gdx.graphics.getHeight();
+        ASPECT_RATIO = (float) Gdx.graphics.getWidth() / (float) Gdx.graphics.getHeight();
         CAMERA_WIDTH = CAMERA_HEIGHT * ASPECT_RATIO;
     }
 
@@ -107,21 +208,31 @@ public class Simulator extends ApplicationAdapter
         shapeRenderer.setColor(Color.GRAY);
         shapeRenderer.rect(cam.position.x - cam.viewportWidth / 2, cam.position.y - cam.viewportHeight / 2, cam.viewportWidth, cam.viewportHeight);
         shapeRenderer.end();*/
-        world.step(1/60f, 6, 2);
+        world.step(1 / 120f, 6, 2);
 
         batch.setProjectionMatrix(cam.combined);
         batch.begin();
 
-        Propeller.getProp1(cam, world).sprite.setRotation((float) Math.toDegrees(Propeller.getProp1(cam, world).body.getAngle()));
-        Propeller.getProp2(cam, world).sprite.setRotation((float) Math.toDegrees(Propeller.getProp2(cam, world).body.getAngle()));
-        Propeller.getProp3(cam, world).sprite.setRotation((float) Math.toDegrees(Propeller.getProp2(cam, world).body.getAngle()));
-        Propeller.getProp4(cam, world).sprite.setRotation((float) Math.toDegrees(Propeller.getProp3(cam, world).body.getAngle()));
-        
-		Propeller.getProp1(cam, world).sprite.draw(batch);
-        Propeller.getProp2(cam, world).sprite.draw(batch);
-        Propeller.getProp3(cam, world).sprite.draw(batch);
-        Propeller.getProp4(cam, world).sprite.draw(batch);
-        
+        Propeller prop1 = Propeller.getProp1(cam, world);
+        Propeller prop2 = Propeller.getProp2(cam, world);
+        Propeller prop3 = Propeller.getProp3(cam, world);
+        Propeller prop4 = Propeller.getProp4(cam, world);
+
+        prop1.sprite.setRotation((float) Math.toDegrees(prop1.body.getAngle()));
+        prop2.sprite.setRotation((float) Math.toDegrees(prop2.body.getAngle()));
+        prop3.sprite.setRotation((float) Math.toDegrees(prop2.body.getAngle()));
+        prop4.sprite.setRotation((float) Math.toDegrees(prop3.body.getAngle()));
+
+        prop1.sprite.draw(batch);
+        prop2.sprite.draw(batch);
+        prop3.sprite.draw(batch);
+        prop4.sprite.draw(batch);
+
+        debugFont.draw(batch, prop1.joint.getMotorSpeed() + ""/*TODO alloc*/, (prop1.x + PROPELLER_HEIGHT) * 1.1f, (prop1.y + PROPELLER_HEIGHT) * 1.1f);
+        debugFont.draw(batch, prop1.joint.getMotorSpeed() + ""/*TODO alloc*/, (prop2.x + PROPELLER_HEIGHT) * 1.1f, (prop2.y + PROPELLER_HEIGHT) * 1.1f);
+        debugFont.draw(batch, prop1.joint.getMotorSpeed() + ""/*TODO alloc*/, (prop3.x + PROPELLER_HEIGHT) * 1.1f, (prop3.y + PROPELLER_HEIGHT) * 1.1f);
+        debugFont.draw(batch, prop1.joint.getMotorSpeed() + ""/*TODO alloc*/, (prop4.x + PROPELLER_HEIGHT) * 1.1f, (prop4.y + PROPELLER_HEIGHT) * 1.1f);
+
         batch.end();
         debugRenderer.render(world, cam.combined);
     }
@@ -147,21 +258,21 @@ public class Simulator extends ApplicationAdapter
 
     public static void draw(SpriteBatch batch, Texture texture, float x, float y, float height)
     {
-        batch.draw(texture, x, y, height * texture.getWidth()/texture.getHeight(), height);
+        batch.draw(texture, x, y, height * texture.getWidth() / texture.getHeight(), height);
     }
 
     public static void draw(SpriteBatch batch, TextureRegion region, float x, float y, float height)
     {
-        batch.draw(region, x, y, height * region.getRegionWidth()/region.getRegionHeight(), height);
+        batch.draw(region, x, y, height * region.getRegionWidth() / region.getRegionHeight(), height);
     }
-	
-	public void setSpeedAll(float speed)
-	{
-		Propeller.getProp1(cam, world).joint.setMotorSpeed(speed);
-		Propeller.getProp2(cam, world).joint.setMotorSpeed(speed);
-		Propeller.getProp3(cam, world).joint.setMotorSpeed(speed);
-		Propeller.getProp4(cam, world).joint.setMotorSpeed(speed);
-	}
+
+    public void setSpeedAll(float speed)
+    {
+        Propeller.getProp1(cam, world).joint.setMotorSpeed(speed);
+        Propeller.getProp2(cam, world).joint.setMotorSpeed(speed);
+        Propeller.getProp3(cam, world).joint.setMotorSpeed(speed);
+        Propeller.getProp4(cam, world).joint.setMotorSpeed(speed);
+    }
 
     private static class Propeller
     {
@@ -169,7 +280,7 @@ public class Simulator extends ApplicationAdapter
 
         float x, y;
         Body body;
-		RevoluteJoint joint;
+        RevoluteJoint joint;
         Sprite sprite;
 
         public Propeller(float x, float y, World world)
@@ -179,18 +290,20 @@ public class Simulator extends ApplicationAdapter
             updateBody(world);
             sprite = new Sprite(propellerTexture);
             sprite.setSize(PROPELLER_HEIGHT, PROPELLER_HEIGHT * sprite.getHeight() / sprite.getWidth());
-            sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
+            sprite.setOrigin(sprite.getWidth() / 2, sprite.getHeight() / 2);
             sprite.setPosition(x, y);
         }
 
         public void updateBody(World world)
         {
-            if(body == null)
+            if (body == null)
             {
                 BodyDef bodyDef = new BodyDef();
 
                 bodyDef.type = BodyDef.BodyType.DynamicBody;
                 bodyDef.position.set(x, y);
+                bodyDef.bullet = true;
+                bodyDef.allowSleep = false;
 
                 Body body = world.createBody(bodyDef);
                 body.setTransform(x + PROPELLER_HEIGHT / 2, y + PROPELLER_HEIGHT / 2, 0);
@@ -200,7 +313,7 @@ public class Simulator extends ApplicationAdapter
                 FixtureDef fixtureDef = new FixtureDef();
                 fixtureDef.shape = shape;
                 fixtureDef.density = 1.0f;
-                fixtureDef.friction = 0.5f;
+                fixtureDef.friction = 1f;
                 fixtureDef.restitution = 0.8f;
 
                 body.createFixture(fixtureDef);
@@ -219,6 +332,7 @@ public class Simulator extends ApplicationAdapter
             else
             {
                 body.getPosition().set(x, y);
+                sprite.setPosition(x, y);
             }
         }
 
@@ -243,7 +357,7 @@ public class Simulator extends ApplicationAdapter
 
         public static Propeller getProp1(OrthographicCamera cam, World world)
         {
-            if(prop1 == null)
+            if (prop1 == null)
             {
                 prop1 = new Propeller(cam.viewportWidth / 2 - 3, cam.viewportHeight / 2 + 1, world);
             }
@@ -258,7 +372,7 @@ public class Simulator extends ApplicationAdapter
 
         public static Propeller getProp2(OrthographicCamera cam, World world)
         {
-            if(prop2 == null)
+            if (prop2 == null)
             {
                 prop2 = new Propeller(cam.viewportWidth / 2 - 3, cam.viewportHeight / 2 - 1 - PROPELLER_HEIGHT, world);
             }
@@ -273,7 +387,7 @@ public class Simulator extends ApplicationAdapter
 
         public static Propeller getProp3(OrthographicCamera cam, World world)
         {
-            if(prop3 == null)
+            if (prop3 == null)
             {
                 prop3 = new Propeller(cam.viewportWidth / 2 + 3 - PROPELLER_HEIGHT, cam.viewportHeight / 2 + 1, world);
             }
@@ -288,7 +402,7 @@ public class Simulator extends ApplicationAdapter
 
         public static Propeller getProp4(OrthographicCamera cam, World world)
         {
-            if(prop4 == null)
+            if (prop4 == null)
             {
                 prop4 = new Propeller(cam.viewportWidth / 2 + 3 - PROPELLER_HEIGHT, cam.viewportHeight / 2 - 1 - PROPELLER_HEIGHT, world);
             }
